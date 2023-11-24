fc4cb692781fe4325ad81626c2de9da3fafc8e62
vansh choubey <vanshchoubey999@gmail.com>
2023-11-24 23:49:58 +0530

    vansh

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="bg-gray-800 text-gray-300 py-6 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center">
+          <div className="text-sm">
+            <p>&copy; {new Date().getFullYear()} Book Renting Website. All rights reserved.</p>
+            <p>Designed and developed by Your Company</p>
+          </div>
+          <div className="flex space-x-4">
+            <a href="#" className="hover:text-gray-400">
+              About Us
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              Contact Us
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              Terms of Service
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
